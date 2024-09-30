02771d523439fed0f81733ec53b16b6812b53aed
julioSilvaDias <juliocesar.silvadi@elorrieta-errekamari>
2024-09-30 22:25:57 +0200

    .

--- Ejercicio1_9/src/ejercicio1_9/Ejercicio1_9.java
+++ Ejercicio1_9/src/ejercicio1_9/Ejercicio1_9.java
@@ -1,10 +1,16 @@
 package ejercicio1_9;
 
 import java.io.File;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -16,6 +22,8 @@ public class Ejercicio1_9 {
 
 	public static void main(String[] args) {
 		leerXml();
+		crearCD();
+		asignarNumeros();
 	}
 
 	public static void leerXml() {
@@ -31,7 +39,6 @@ public class Ejercicio1_9 {
 			for (int i = 0; i < nList.getLength(); i++) {
 				Node nNode = nList.item(i);
 				System.out.print(nNode.getNodeName());
-			
 
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element) nNode;
@@ -81,22 +88,103 @@ public class Ejercicio1_9 {
 		}
 	}
 
-	public static void creaCD() {
-		
+	public static void asignarNumeros() {
 		try {
 			File file = new File(RUTA);
-			DocumentBuilderFactory dbFactory = 	DocumentBuilderFactory.newInstance();
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(file);
-			
-			Node catalogo = doc.getFirstChild();
-			
-				
-			
+			doc.getDocumentElement().normalize();
+
+			NodeList listaCDs = doc.getElementsByTagName("CD");
+
+			for (int i = 0; i < listaCDs.getLength(); i++) {
+				Node cd = listaCDs.item(i);
+
+				if (cd.getNodeType() == Node.ELEMENT_NODE) {
+					Element elementoCD = (Element) cd;
+					elementoCD.setAttribute("numero", String.valueOf(i + 1));
+				}
+			}
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(file);
+			transformer.transform(source, result);
+
+			System.out.println("Atributo 'numero' añadido a los CDs correctamente.");
+
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+	}
+
+	public static void crearCD() {
+
+		try {
+			Scanner teclado = new Scanner(System.in);
+			String txt = "";
+			File file = new File(RUTA);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(file);
+			doc.getDocumentElement().normalize();
+
+			Node catalogo = doc.getFirstChild();
+
+			Element nuevoCD = doc.createElement("CD");
+
+			System.out.print("Introduce el titulo: ");
+			txt = teclado.nextLine();
+			Element titulo = doc.createElement("TITLE");
+			titulo.appendChild(doc.createTextNode(txt));
+			nuevoCD.appendChild(titulo);
+
+			System.out.print("Introduce el artista: ");
+			txt = teclado.nextLine();
+			Element artista = doc.createElement("ARTIST");
+			artista.appendChild(doc.createTextNode(txt));
+			nuevoCD.appendChild(artista);
+
+			System.out.print("Introduce el país: ");
+			txt = teclado.nextLine();
+			Element pais = doc.createElement("COUNTRY");
+			pais.appendChild(doc.createTextNode(txt));
+			nuevoCD.appendChild(pais);
+
+			System.out.print("Introduce la compania: ");
+			txt = teclado.nextLine();
+			Element compania = doc.createElement("COMPANY");
+			compania.appendChild(doc.createTextNode(txt));
+			nuevoCD.appendChild(compania);
+
+			System.out.print("Introduce el precio: ");
+			txt = teclado.nextLine();
+			Element precio = doc.createElement("PRICE");
+			precio.appendChild(doc.createTextNode(txt));
+			nuevoCD.appendChild(precio);
+
+			System.out.print("Introduce el año: ");
+			txt = teclado.nextLine();
+			Element anio = doc.createElement("YEAR");
+			anio.appendChild(doc.createTextNode(txt));
+			nuevoCD.appendChild(anio);
+			catalogo.appendChild(nuevoCD);
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(file);
+			transformer.transform(source, result);
+
+			System.out.println("Nuevo CD añadido correctamente.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
